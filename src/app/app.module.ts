e4c39c9613c2e2f28ea530e8043238e9b973fca2
Wenfeng Luo <luo2372@gmail.com>
2017-11-15 18:19:26 -0500

    tmp1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule,
+  MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule,
+  MatSlideToggleModule, MatToolbarModule, MatListModule, MatGridListModule,
+  MatCardModule, MatIconModule, MatProgressSpinnerModule, MatDialogModule, MatChipsModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AgmCoreModule } from '@agm/core';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule,
+      MatInputModule, MatRadioModule, MatSelectModule, MatSliderModule,
+      MatSlideToggleModule, MatToolbarModule, MatListModule, MatGridListModule,
+      MatCardModule, MatIconModule, MatProgressSpinnerModule, MatDialogModule, MatChipsModule,
+    FlexLayoutModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBwyhyXYztX3BZsVpqmGIYMcIOak-x5-js'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
